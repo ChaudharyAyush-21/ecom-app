2c31cf4ff93aad6699fce9b556fa8c78bb8a394e
ChaudharyAyush-21 <ayushk2194@gmail.com>
2025-07-28 22:41:25 +0530

    adding the product data

--- Frontened/src/pages/Home.jsx
+++ Frontened/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import BestSellar from '../components/BestSellar'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSellar />
     </div>
   )
 }
