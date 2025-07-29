43d303c19c6897477e665fcdf6a031177545ae3b
ChaudharyAyush-21 <ayushk2194@gmail.com>
2025-07-29 21:38:45 +0530

    adding the search functionality

--- Frontened/src/components/Navbar.jsx
+++ Frontened/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Navbar = () => {
 
-  const[visible, setvisible] = useState(false)
+  const[visible, setVisible] = useState(false);
+  const {setshowSearch} = useContext(ShopContext);
 
   return (
     <div className='flex justify-between items-center py-5 font-medium'>
@@ -28,7 +30,7 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className='flex items-center gap-6'>
-        <img src={assets.search_icon} alt="" className='w-5 cursor-pointer' />
+        <img onClick={()=>setshowSearch(true)} src={assets.search_icon} alt="" className='w-5 cursor-pointer' />
         <div className='group relative'>
           <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
@@ -43,18 +45,18 @@ const Navbar = () => {
           <img src={assets.cart_icon} alt="" className='w-5 min-w-5 '/>
           <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
         </Link>
-        <img onClick={()=>setvisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
+        <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
       </div>
       <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' :'w-0'}`} >
           <div className='flex flex-col text-gray-600'>
-              <div onClick={()=>setvisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+              <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
                 <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
                 <p>Back</p>
               </div>
-              <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
-              <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTIONS</NavLink>
-              <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
-              <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTIONS</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
           </div>
       </div>
     </div>
--- Frontened/src/pages/Collection.jsx
+++ Frontened/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
-  const {products} =useContext(ShopContext);
+  const {products ,search , showsearch} =useContext(ShopContext);
   const [showFilter , setShowFilter] = useState(false);
   const [filterProducts, setfilterProducts] = useState([]);
   const [category ,setCategory] =useState([]);
@@ -33,6 +33,11 @@ const Collection = () => {
 
   const applyFilter =()=>{
     let productCopy = products.slice();
+
+    if(showsearch && search){
+      productCopy = productCopy.filter(item =>item.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
     if(category.length >0){
       productCopy = productCopy.filter(item => category.includes(item.category));
     }
@@ -65,7 +70,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
-  },[category,subCategory])
+  },[category, subCategory, search, showsearch])
 
   useEffect(()=>{
     shortProduct();
