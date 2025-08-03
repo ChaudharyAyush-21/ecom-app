ad14df0aa0e2cd9a79cbe37d53fa8cacacff4060
ChaudharyAyush-21 <ayushk2194@gmail.com>
2025-08-03 16:28:44 +0530

    done with the product page by adding related product

--- Frontened/src/components/RelatedProducts.jsx
+++ Frontened/src/components/RelatedProducts.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+import ProductItem from './ProductItem';
 
-const RelatedProducts = () => {
+const RelatedProducts = ({category,subCategory}) => {
+
+  const {products} = useContext(ShopContext);
+  const [related ,setRelated] = useState([]);
+
+  useEffect(()=>{
+    if(products.length >0){
+      let productsCopy = products.slice();
+      productsCopy = productsCopy.filter((item)=>category === item.category);
+      productsCopy = productsCopy.filter((item)=>subCategory === item.subCategory);
+      setRelated(productsCopy.slice(0,5));
+    }
+  },[products])
+
   return (
-    <div>
-      
+    <div className='my-24'>
+      <div className='text-center text-3xlpy-2'>
+        <Title text1={'Related'} text2={'Products'}/>
+      </div>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:gride-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {related.map((item,index)=>(
+            <ProductItem key={index} image={item.image} id={item._id} name={item.name} price={item.price}/>
+        )
+      )}
+      </div>
     </div>
   )
 }
--- Frontened/src/pages/Product.jsx
+++ Frontened/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
@@ -84,6 +85,7 @@ const Product = () => {
         </div>
       </div>
       {/* display related products */}
+      <RelatedProducts category={productdata.category} subCategory={productdata.subCategory} />
     </div>
   ) : <div className='opacity-0'></div>
 }
