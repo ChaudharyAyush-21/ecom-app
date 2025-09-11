2c0a66fb6ffd6f8455036e9e45b685bbcb667146
ChaudharyAyush-21 <ayushk2194@gmail.com>
2025-09-11 23:57:49 +0530

    adding admin data to frontend

--- Frontened/src/context/ShopContext.jsx
+++ Frontened/src/context/ShopContext.jsx
@@ -1,17 +1,19 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/assets";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
-    const currency = '$';
+    const currency = '₹';
     const delivery_fee = 10;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [search , setSearch]= useState('');
     const [showsearch , setshowSearch] = useState(false);
     const [cartitems, setCartitems] = useState({});
+    const [products, setProducts] = useState([])
     const navigate =  useNavigate();
 
     const addToCart = async(itemId, size) =>{
@@ -74,8 +76,26 @@ const ShopContextProvider = (props) => {
         return totalAmount;
     }
 
+    const getProductsData = async () => {
+        try {
+            const response = await axios.get(backendUrl + '/api/product/list')
+            if(response.data.success){
+                setProducts(response.data.products)
+            }else{
+                toast.error(response.data.message)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+            
+        }
+    }
+    useEffect(()=>{
+        getProductsData()
+    },[])
+
     const value ={
-        products, currency, delivery_fee, search, setSearch, showsearch, setshowSearch, cartitems, addToCart, getCartcount, updateQuantity, getCartAmount, navigate
+        products, currency, delivery_fee, search, setSearch, showsearch, setshowSearch, cartitems, addToCart, getCartcount, updateQuantity, getCartAmount, navigate, backendUrl
     }
     return(
         <ShopContext.Provider value={value}>
